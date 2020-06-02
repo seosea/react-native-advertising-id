a88fa8a4ee85bf3e1929095ac8e4432ca98da2e8
seose <ssyaoao@naver.com>
2020-06-02 15:15:43 +0900

    sfa

---
+++ ios/RNAdvertisingId.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  RNAdvertisingId
+//
+//  Created by seo on 2020/06/02.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
